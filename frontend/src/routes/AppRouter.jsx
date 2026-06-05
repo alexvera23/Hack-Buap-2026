3865c37fd9f77a1de5e48d17a01e45b4607202b9
Naomis079 <naomi.oterof@alumno.buap.mx>
2026-06-05 10:30:19 -0600

    nueva estructura

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../views/Home/Home'
+import Agriculture from '../views/Agriculture/Agriculture'
+import AnimalCare from '../views/AnimalCare/AnimalCare'
+import Health from '../views/Health/Health'
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agriculture" element={<Agriculture />} />
+        <Route path="/animal-care" element={<AnimalCare />} />
+        <Route path="/health" element={<Health />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
