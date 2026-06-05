ef0bfea657f09e81e8193b4ac1407ce0e4e3a82e
Naomis079 <naomi.oterof@alumno.buap.mx>
2026-06-05 12:08:36 -0600

    correccion de front

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Listo para v4
-    'autoprefixer': {},
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
 }
